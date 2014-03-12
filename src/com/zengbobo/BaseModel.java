534a161e1983c2683646e2224139c94d99140c49
zengbobo0805 <boy812796958@aliyun.com>
2014-03-12 17:59:48 +0800

    add BaseModel.java

---
+++ src/com/zengbobo/BaseModel.java
@@ -0,0 +1,20 @@
+package com.zengbobo;
+
+public class BaseModel {
+	private String name;
+	private String old;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOld() {
+		return old;
+	}
+	public void setOld(String old) {
+		this.old = old;
+	}
+	
+	
+}
