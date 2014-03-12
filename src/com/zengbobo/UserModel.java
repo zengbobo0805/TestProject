ce9ae2496c26ad4000699df13f80d428c213f6c1
zengbobo0805 <boy812796958@aliyun.com>
2014-03-12 18:14:30 +0800

    usermodel

---
+++ src/com/zengbobo/UserModel.java
@@ -0,0 +1,15 @@
+package com.zengbobo;
+
+public class UserModel extends BaseModel {
+	private int code ;
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	
+}
